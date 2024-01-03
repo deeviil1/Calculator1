dab2b99c4bb0a5aeb9c3b5f3306f066cd53a7c65
deeviil1 <uraandrosenko@gmail.com>
2024-01-03 17:45:41 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,57 @@
 import java.util.Scanner;
 
 public class Main {
+     static int num;
+
+     static int num2;
+
+     static int result;
+
+     static char oper;
+
     public static void main(String[] args) {
 
-
-        int num ;
-
-        int num2 ;
-
-        int result;
-
-        char oper;
 
         Scanner sc = new Scanner(System.in);
         String string = sc.next();
         String[] str = string.split("/, *, -, +");
         if (string.contains("*")) {
             oper = '*';
+        } else if (string.contains("/")) {
+            oper = '/';
+
+        } else if (string.contains("-")) {
+            oper = '-';
+        } else if (string.contains("+")) {
+            oper = '+';
+
         }
-           else if (string.contains("/")){
-               oper = '/';
+
+
+        num = Integer.parseInt(string);
+        num2 = Integer.parseInt(string);
+    }
+
+
+        public static void action ( int num, int num2, int result, char oper){
+
+            switch (oper) {
+                case '+':
+                    result = num + num2;
+                    break;
+                case '-':
+                    result = num - num2;
+                    break;
+                case '*':
+                    result = num * num2;
+                    break;
+                case '/':
+                    result = num / num2;
+                    break;
 
             }
-           else if (string.contains("-")) {
-               oper ='-';
-        } else if (string.contains("+")) {
-               oper = '+';
-
-        }
-
-
-        }
-
-        public void action(int num, int num2, int result, char oper){
-
-        switch(oper){
-            case '+':
-                result = num + num2;
-                break;
-            case'-':
-                result = num - num2;
-                break;
-            case '*':
-                result = num * num2;
-                break;
-            case '/':
-                result = num/num2;
-                break;
-
-        }
         }
 
 
     }
+

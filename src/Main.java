3ec912f94c0f7e27a2100baf25d5f36834d1d57c
deeviil1 <uraandrosenko@gmail.com>
2024-01-05 14:20:04 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -3,69 +3,75 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Integer res = null;
 
+        Integer res = null;
 
         Scanner sc = new Scanner(System.in);
 
 
         while (true) {
-            String string = sc.nextLine();
+        String string = sc.nextLine();
             if (string.equals("End")) {
                 System.out.println("Приложение закончило работу");
-
+                break;
             }
 
-            try {
-                String[] str = string.split(" ");
 
+            String[] str = string.split(" ");
+            try {
                 check(str);
 
 
-                if (str[1].equals("+")) {
-                    res = Integer.parseInt(str[0]) + Integer.parseInt(str[2]);
-
-                }
-
-                if (str[1].equals("-")) {
-                    res = Integer.parseInt(str[0]) - Integer.parseInt(str[2]);
-
-                }
-                if (str[1].equals("*")) {
-                    res = Integer.parseInt(str[0]) * Integer.parseInt(str[2]);
-                }
-                if (str[1].equals("/")) {
-                    res = Integer.parseInt(str[0]) / Integer.parseInt(str[2]);
-                }
-
-                System.out.println(res);
 
 
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+        if (str[1].equals("+")) {
+            res = Integer.parseInt(str[0]) + Integer.parseInt(str[2]);
 
-
-            }
         }
+
+        if (str[1].equals("-")) {
+            res = Integer.parseInt(str[0]) - Integer.parseInt(str[2]);
+
+        }
+        if (str[1].equals("*")) {
+            res = Integer.parseInt(str[0]) * Integer.parseInt(str[2]);
+        }
+        if (str[1].equals("/")) {
+            res = Integer.parseInt(str[0]) / Integer.parseInt(str[2]);
+        }
+
+        System.out.println(res);
+
+        } catch (Exception e) {
+                System.out.println(e.getMessage());
+        }
+
+
     }
 
+    }
 
     public static void check(String[] strings) throws Exception {
 
-        if (strings.length != 3) {
-            throw new Exception("невнрная длинна выражения");
+        if (strings.length!=3){
+            throw new Exception("Неверное количество символов");
         }
-       if (!"+-*/".equals(strings )){
-          throw new Exception("не верный символ");
-      }
-
-       if (!"12345678910".equals(strings)) {
-          throw new Exception("не являеться числом");
+        if (!"+-*/".equals(strings)){
+            throw new Exception("неверный арфметческий знак");
+        }
+        if ("12345678910".equals(strings)){
+            throw new Exception("символ не является числом");
         }
 
 
     }
 }
+
+
+
+
+
+
 
 
 
